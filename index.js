a466742ae76334d7658f8ea7dc5c06216757b644
Tiago <t.a.marques@hotmail.com>
2025-10-22 18:33:30 +0100

    feat(be): date endpoint to make date strings into unix format

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,12 +14,26 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/:date?", function (req, res) {
+  const { date: dateParam } = req.params;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  if (!dateParam) {
+    date = new Date();
+  } else
+    if (/^\d+$/.test(dateParam)) {
+      date = dateParam.length === 10
+        ? new Date(Number(dateParam) * 1000)
+        : new Date(Number(dateParam));
+    } else {
+      date = new Date(dateParam)
+    }
+
+  if (date.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({ unix: date.getTime() })
 });
-
 
 
 // Listen on port set in environment variable or default to 3000
