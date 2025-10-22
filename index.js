948f4905ad6e0d5e73c262ae1b665328e4eaa4cc
Tiago <t.a.marques@hotmail.com>
2025-10-22 20:58:57 +0100

    feat(be): date endpoint to make unix format into utc

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ var app = express();
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -32,7 +30,10 @@ app.get("/api/:date?", function (req, res) {
     return res.json({ error: "Invalid Date" });
   }
 
-  res.json({ unix: date.getTime() })
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  })
 });
 
 
